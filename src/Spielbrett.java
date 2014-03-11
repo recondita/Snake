4b1ab48202d86d5006fcc51cf9fd0015a59abce0
felix <felix@familie-schuetze.de>
2014-03-11 17:19:21 +0100

    Bilder
    und hör auf meine Klassen zu löschen

--- src/Spielbrett.java
+++ src/Spielbrett.java
@@ -1,5 +1,83 @@
 import javax.swing.JPanel;
-public class Spielbrett extends JPanel {
-	private static final long serialVersionUID = 1L;
 
+public class Spielbrett extends JPanel
+{
+	private static final long serialVersionUID = 1L;
+	int breite=40;
+	int hoehe=30;
+	int[][] feld = new int[breite][hoehe];
+	
+	/*
+	 * 0=nichts
+	 * 1=Apfel
+	 * 10=Schlangenkopf
+	 * 	11=Schlangenkopf nach oben
+	 * 	12=Schlangenkopf nach rechts
+	 * 	13=Schlangenkopf nach unten
+	 * 	14=Schlangenkopf nach links
+	 * 20=Schlangenschwanz
+	 * 	21=Schlangenschwanz nach oben
+	 * 	22=Schlangenschwanz nach rechts
+	 * 	23=Schlangenschwanz nach unten
+	 * 	24=Schlangenschwanz nach links
+	 * 30=Schlange
+	 * 	31=Schlange nach oben
+	 * 	32=Schlange nach rechts
+	 * 	33=Schlange nach unten
+	 * 	34=Schlange nach links
+	 */
+	
+	public Spielbrett()
+	{
+		for(int i=0;i<breite;i++)
+		{
+			for(int j=0;j<hoehe;j++)
+			{
+				feld[i][j]=0;
+			}
+		}
+		neuerApfel();
+	}
+	
+	public void neuerApfel()
+	{
+		if(!voll())
+		{
+			int x=(int)(Math.random()*breite+1);
+			int y=(int)(Math.random()*hoehe+1);
+			if(feld[x][y]==0)
+			{
+				feld[x][y]=1;
+			}
+		}
+	}
+	
+	public boolean voll()
+	{
+		for(int i=0;i<breite;i++)
+		{
+			for(int j=0;j<hoehe;j++)
+			{
+				if(feld[i][j]==0)
+				{
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	public void anzeigen()
+	{
+		for(int i=0;i<breite;i++)
+		{
+			for(int j=0;j<hoehe;j++)
+			{
+				if(feld[i][j]==0)
+				{
+					
+				}
+			}
+		}
+	}
 }
