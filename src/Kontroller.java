667b3c5031647d3b11c055276d8372df364993c2
Jan <jan.hofmeier@yahoo.de>
2014-03-11 16:17:03 +0100

    Projekt

---
+++ src/Kontroller.java
@@ -0,0 +1,16 @@
+
+public class Kontroller
+{
+
+	public Kontroller()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
